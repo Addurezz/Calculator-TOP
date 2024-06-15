bcf610a5658c21285fe153074d7668cb75a3ecea
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2024-06-15 20:03:00 +0200

    add basic functions (plus, minus etc...)

---
+++ script.js
@@ -0,0 +1,21 @@
+//calculator functions
+
+const addition = (a,b) => {
+    return a + b;
+}
+
+const subtraction = (a,b) => {
+    return a - b;
+}
+
+const multiplication = (a,b) => {
+    return a * b;
+}
+
+const division = (a,b) => {
+    if (b===0) {
+        return "ERROR"
+    }
+    return a/b;
+}   
+
