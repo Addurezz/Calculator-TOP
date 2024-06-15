0875d8e9f7df56604ac6eade537ef63acbde6a8e
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2024-06-15 20:32:43 +0200

    remove basic functions because of refactoring

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//calculator functions
-
-const addition = (a,b) => {
-    return a + b;
-}
-
-const subtraction = (a,b) => {
-    return a - b;
-}
-
-const multiplication = (a,b) => {
-    return a * b;
-}
-
-const division = (a,b) => {
-    if (b===0) {
-        return "ERROR"
-    }
-    return a/b;
-}   
-
 //display pressed buttons on screen
 
 const output = document.querySelector("input");
@@ -27,11 +6,27 @@ const displayItems = (item) => {
     output.value += item.textContent;
 }
 
-// 
-
 const button = document.querySelectorAll(".button");
 
 button.forEach((element) => {
     const userInput = element;
     element.addEventListener("click", () => displayItems(element))
 })
+
+//clear the screen 
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => output.value = "")
+
+//get variables a and b 
+
+const getVariableA = () => {
+
+}
+
+
+
+
+
+
+
